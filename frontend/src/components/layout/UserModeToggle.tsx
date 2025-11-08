56b1af8322f61f6b7a3ddc21306f3a224ae1fc5e
tkn1223 <taka12230000@gmail.com>
2025-11-08 11:00:16 +0900

    couple_idがnullの時、usermodeの変更が行えないように変更

--- frontend/src/components/layout/UserModeToggle.tsx
+++ frontend/src/components/layout/UserModeToggle.tsx
@@ -2,27 +2,43 @@
 
 import { User, Users } from "lucide-react";
 import { Toggle } from "@/components/ui/toggle.tsx";
+import { useAuth } from "@/contexts/AuthContext.tsx";
 import type { UserModeToggleProps } from "@/types/viewmode";
 
 export function UserModeToggle({ user, setUser }: UserModeToggleProps) {
-  return (
-    <Toggle
-      aria-label="Toggle user mode"
-      onClick={() => setUser(user === "alone" ? "common" : "alone")}
-    >
-      <div className="flex items-center justify-center w-full h-full cursor-pointer">
-        {user === "alone" ? (
-          <>
-            <User className="h-4 w-4 mr-1" />
-            個人
-          </>
-        ) : (
-          <>
-            <Users className="h-4 w-4 mr-1" />
-            共通
-          </>
-        )}
-      </div>
-    </Toggle>
-  );
+  const { userInfo } = useAuth();
+
+  if (userInfo.couple_id !== "null") {
+    return (
+      <Toggle
+        aria-label="Toggle user mode"
+        onClick={() => setUser(user === "alone" ? "common" : "alone")}
+      >
+        <div className="flex items-center justify-center w-full h-full cursor-pointer">
+          {user === "alone" ? (
+            <>
+              <User className="h-4 w-4 mr-1" />
+              個人
+            </>
+          ) : (
+            <>
+              <Users className="h-4 w-4 mr-1" />
+              共通
+            </>
+          )}
+        </div>
+      </Toggle>
+    );
+  } else {
+    return (
+      <Toggle
+        aria-label="Toggle user mode"
+        disabled
+        className="cursor-not-allowed"
+      >
+        <User className="h-4 w-4 mr-1" />
+        個人
+      </Toggle>
+    );
+  }
 }
