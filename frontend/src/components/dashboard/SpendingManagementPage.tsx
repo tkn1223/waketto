9d102b65d5feb0b92a5561124d2c3f72f7cf0be5
tkn1223 <taka12230000@gmail.com>
2025-11-29 22:37:33 +0900

    予算消化状況の使い方説明を追加

--- frontend/src/components/dashboard/SpendingManagementPage.tsx
+++ frontend/src/components/dashboard/SpendingManagementPage.tsx
@@ -1,6 +1,12 @@
+import { CircleQuestionMark } from "lucide-react";
 import { BudgetUsageList } from "@/components/dashboard/BudgetUsage/BudgetUsageList.tsx";
 import { ExpenseTable } from "@/components/dashboard/ExpenseReport/ExpenseTable.tsx";
 import { TransactionDetail } from "@/components/dashboard/Transaction/TransactionDetail.tsx";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip.tsx";
 import { YearMonthSelector } from "@/components/ui/YearMonthSelector.tsx";
 import { useBudgetUsage, useExpenseReport } from "@/lib/swr.ts";
 import type { SpendingManagementPageProps } from "@/types/summary.ts";
@@ -48,7 +54,22 @@ export function SpendingManagementPage({
       {/* 予算消化率 */}
       <div className="order-3 lg:order-3 sm:col-span-3 lg:col-span-3 space-y-3">
         <div className="flex justify-between">
-          <span className="font-medium text-lg">予算の消化状況</span>
+          <div className="flex items-center gap-3">
+            <span className="font-medium text-lg">予算の消化状況</span>
+            {/* クエスチョンアイコン */}
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <CircleQuestionMark className="text-gray-400 w-5" />
+              </TooltipTrigger>
+              <TooltipContent className="p-3 text-sm">
+                <p>
+                  予算設定表で「1年」を選択した項目に対して
+                  <br />
+                  年間の予算消化状況を表示します
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
           <YearMonthSelector {...yearlyDateSelector} showMonth={false} />
         </div>
         <BudgetUsageList budgetUsage={budgetUsage} />
