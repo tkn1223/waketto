b605a1c34077d2686a86072ab71ecd0a0a6c8396
tkn1223 <taka12230000@gmail.com>
2025-11-30 10:04:01 +0900

    簡易的なスマホ表示対応

--- frontend/src/components/setting/BudgetSetting.tsx
+++ frontend/src/components/setting/BudgetSetting.tsx
@@ -7,97 +7,104 @@ export function BudgetSetting({
 }: BudgetSettingProps) {
   return (
     <>
-      <table className="w-full table-auto mt-4">
-        <thead>
-          <tr className="bg-muted">
-            <th className="w-1/13"></th>
-            <th className="w-6/13 py-2 text-lg border-3">固定費</th>
-            <th className="w-6/13 py-2 text-lg border-3">変動費</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="text-lg font-bold text-center bg-muted border-3">
-              毎月
-            </td>
-            <td className="py-4 px-2 border-2 align-top">
-              <BudgetCategoryRow
-                categoryGroup={allCategories.filter(
-                  (category) => category.groupCode === "monthly_fixed_cost"
-                )}
-                onUpdate={handleCategoryUpdate}
-              />
-            </td>
-            <td className="py-4 px-2 border-2 align-top">
-              <div className="grid gap-2">
+      <div className="overflow-x-auto mt-4">
+        <table className="w-full table-auto min-w-[600px]">
+          <thead>
+            <tr className="bg-muted">
+              <th className="w-1/13"></th>
+              <th className="w-6/13 py-2 text-lg border-3">固定費</th>
+              <th className="w-6/13 py-2 text-lg border-3">変動費</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="text-lg font-bold text-center bg-muted border-3">
+                毎月
+              </td>
+              <td className="py-4 px-2 border-2 align-top">
                 <BudgetCategoryRow
                   categoryGroup={allCategories.filter(
-                    (category) => category.groupCode === "monthly_variable_cost"
+                    (category) => category.groupCode === "monthly_fixed_cost"
                   )}
                   onUpdate={handleCategoryUpdate}
                 />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td className="text-lg font-bold text-center bg-muted border-3">
-              不定期
-            </td>
-            <td className="py-4 px-2 border-2 align-top">
-              <BudgetCategoryRow
-                categoryGroup={allCategories.filter(
-                  (category) => category.groupCode === "occasional_fixed_cost"
-                )}
-                onUpdate={handleCategoryUpdate}
-              />
-            </td>
-            <td className="py-4 px-2 border-2 align-top">
-              <BudgetCategoryRow
-                categoryGroup={allCategories.filter(
-                  (category) =>
-                    category.groupCode === "occasional_variable_cost"
-                )}
-                onUpdate={handleCategoryUpdate}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              </td>
+              <td className="py-4 px-2 border-2 align-top">
+                <div className="grid gap-2">
+                  <BudgetCategoryRow
+                    categoryGroup={allCategories.filter(
+                      (category) =>
+                        category.groupCode === "monthly_variable_cost"
+                    )}
+                    onUpdate={handleCategoryUpdate}
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="text-lg font-bold text-center bg-muted border-3">
+                不定期
+              </td>
+              <td className="py-4 px-2 border-2 align-top">
+                <BudgetCategoryRow
+                  categoryGroup={allCategories.filter(
+                    (category) => category.groupCode === "occasional_fixed_cost"
+                  )}
+                  onUpdate={handleCategoryUpdate}
+                />
+              </td>
+              <td className="py-4 px-2 border-2 align-top">
+                <BudgetCategoryRow
+                  categoryGroup={allCategories.filter(
+                    (category) =>
+                      category.groupCode === "occasional_variable_cost"
+                  )}
+                  onUpdate={handleCategoryUpdate}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-      <table className="w-full table-auto mt-4">
-        <thead>
-          <tr>
-            <th className="w-1/13"></th>
-            <th className="w-6/13 py-2 text-lg bg-muted border-3">
-              豊かな浪費
-            </th>
-            <th className="w-6/13 py-2 text-lg bg-muted border-3">
-              貯蓄・投資
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td className="py-2 px-2 border-2">
-              <BudgetCategoryRow
-                categoryGroup={allCategories.filter(
-                  (category) => category.groupCode === "luxury_consumption_cost"
-                )}
-                onUpdate={handleCategoryUpdate}
-              />
-            </td>
-            <td className="py-2 px-2 border-2 align-top">
-              <BudgetCategoryRow
-                categoryGroup={allCategories.filter(
-                  (category) => category.groupCode === "savings_investment_cost"
-                )}
-                onUpdate={handleCategoryUpdate}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="overflow-x-auto mt-4">
+        <table className="w-full table-auto min-w-[600px]">
+          <thead>
+            <tr>
+              <th className="w-1/13"></th>
+              <th className="w-6/13 py-2 text-lg bg-muted border-3">
+                豊かな浪費
+              </th>
+              <th className="w-6/13 py-2 text-lg bg-muted border-3">
+                貯蓄・投資
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td className="py-2 px-2 border-2">
+                <BudgetCategoryRow
+                  categoryGroup={allCategories.filter(
+                    (category) =>
+                      category.groupCode === "luxury_consumption_cost"
+                  )}
+                  onUpdate={handleCategoryUpdate}
+                />
+              </td>
+              <td className="py-2 px-2 border-2 align-top">
+                <BudgetCategoryRow
+                  categoryGroup={allCategories.filter(
+                    (category) =>
+                      category.groupCode === "savings_investment_cost"
+                  )}
+                  onUpdate={handleCategoryUpdate}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
