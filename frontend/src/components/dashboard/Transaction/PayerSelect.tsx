75d7837ae1de6d9f49ce21b4a55bf5e41be5d4a6
tkn1223 <taka12230000@gmail.com>
2025-12-04 00:51:40 +0900

    非有効化の条件にユーザーモードがaloneの時を追加

--- frontend/src/components/dashboard/Transaction/PayerSelect.tsx
+++ frontend/src/components/dashboard/Transaction/PayerSelect.tsx
@@ -1,11 +1,13 @@
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group.tsx";
 import type { PayerSelectProps } from "@/types/transaction.ts";
+import { useViewMode } from "@/contexts/ViewModeContext.tsx";
 
 export function PayerSelect({
   userInfo,
   payer,
   onPayerChange,
 }: PayerSelectProps) {
+  const { user } = useViewMode();
   // 選択された支払者を取得
   const getSelectedPayer = () => {
     if (payer === userInfo.id) {
@@ -39,11 +41,10 @@ export function PayerSelect({
           {userInfo.name}
         </ToggleGroupItem>
         <ToggleGroupItem
-          disabled={!userInfo.couple_id}
+          disabled={user === "alone" || !userInfo.couple_id}
           className={[
             "px-6 py-2 text-sm outline-none transition",
-            // パートナーがいない場合は無効化
-            !userInfo.couple_id
+            user === "alone" || !userInfo.couple_id
               ? "bg-gray-700 text-black cursor-not-allowed"
               : getSelectedPayer() === userInfo.couple_id
                 ? "!bg-cyan-600 !text-white"
