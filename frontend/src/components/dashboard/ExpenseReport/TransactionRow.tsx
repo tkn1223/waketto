82a7f33b6006a45a4fe456268a4a51262350e717
tkn1223 <taka12230000@gmail.com>
2025-12-08 01:05:48 +0900

    フロントエンドのデザインをアップデート

--- frontend/src/components/dashboard/ExpenseReport/TransactionRow.tsx
+++ frontend/src/components/dashboard/ExpenseReport/TransactionRow.tsx
@@ -36,19 +36,37 @@ export function TransactionRow({
     setSelectedPayment(null);
   }, []); // 依存関係なし
 
+  // 非表示条件：budget_amountが0 かつ 実績も0の場合
+  if (category.budget_amount === 0 && totalAmount === 0) {
+    return null;
+  }
+
   return (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value={category.category_name} className="space-y-1.5">
         <AccordionTrigger className="grid grid-cols-12 items-center p-1.5 rounded shadow-sm border-1 border-gray-50 hover:bg-gray-100 hover:border-blue-400 hover:shadow-none hover:no-underline cursor-pointer">
           <span className="col-span-4">{category.category_name}</span>
-          <span className="col-span-4 text-right text-gray-500 text-xs">
-            {category.budget_amount !== null
-              ? `予算 ${category.budget_amount?.toLocaleString()} 円`
-              : ``}
-          </span>
-          <span className="col-span-3 text-right">
-            {totalAmount.toLocaleString()} 円
-          </span>
+          {totalAmount === 0 &&
+          category.budget_amount !== null &&
+          category.budget_amount > 0 ? (
+            <div className="col-span-7 flex items-end justify-end gap-2">
+              <span className="text-gray-500 text-xs">予算</span>
+              <span className="">
+                {category.budget_amount.toLocaleString()} 円
+              </span>
+            </div>
+          ) : (
+            <>
+              <span className="col-span-4 text-right text-gray-500 text-xs">
+                {category.budget_amount !== null
+                  ? `予算 ${category.budget_amount?.toLocaleString()} 円`
+                  : ``}
+              </span>
+              <span className="col-span-3 text-right">
+                {totalAmount.toLocaleString()} 円
+              </span>
+            </>
+          )}
         </AccordionTrigger>
 
         {/* アコーディオンの中身 */}
